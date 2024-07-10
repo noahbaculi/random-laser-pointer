d610c9f1d05b8a36fbaf1676c90c8a8cc0f243e1
Noah Baculi <noahbaculi@gmail.com>
2024-07-09 17:57:36 -0700

    blinky!

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,33 @@
 
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    clock::ClockControl,
+    delay::Delay,
+    gpio::{Io, Level, Output},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
 };
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
-
     let clocks = ClockControl::max(system.clock_control).freeze();
     let delay = Delay::new(&clocks);
 
     esp_println::logger::init_logger_from_env();
 
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = Output::new(io.pins.gpio13, Level::High);
+
     loop {
-        log::info!("Hello world!");
+        led.set_low();
+        log::info!("LED Off");
+        delay.delay(500.millis());
+
+        led.set_high();
+        log::info!("LED On");
         delay.delay(500.millis());
     }
 }
