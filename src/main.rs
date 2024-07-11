48b7287b6050f0513e1d0edb076ae2b1a497cc34
Noah Baculi <noahbaculi@gmail.com>
2024-07-11 08:50:08 -0700

    working random

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use esp_hal::{
     prelude::*,
     system::SystemControl,
 };
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
 
 const SERVO_MIN_DUTY: u8 = 2;
 const SERVO_MAX_DUTY: u8 = 13;
@@ -26,7 +28,6 @@ fn main() -> ! {
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut power_led = Output::new(io.pins.gpio13, Level::High);
-
     power_led.set_high();
     log::info!("Power LED On");
 
@@ -34,11 +35,9 @@ fn main() -> ! {
     let pin_for_servo_2 = io.pins.gpio5;
 
     let mut ledc_pwm_controller = Ledc::new(peripherals.LEDC, &clocks);
-
     ledc_pwm_controller.set_global_slow_clock(LSGlobalClkSource::APBClk);
 
     let mut pwm_timer = ledc_pwm_controller.get_timer::<LowSpeed>(timer::Number::Timer0);
-
     pwm_timer
         .configure(timer::config::Config {
             duty: timer::config::Duty::Duty14Bit,
@@ -47,6 +46,7 @@ fn main() -> ! {
         })
         .unwrap();
 
+    // Instantiate PWM channels
     let mut servo_1_pwm_channel =
         ledc_pwm_controller.get_channel(channel::Number::Channel0, pin_for_servo_1);
     servo_1_pwm_channel
@@ -66,10 +66,14 @@ fn main() -> ! {
         })
         .unwrap();
 
+    let mut small_rng = SmallRng::seed_from_u64(1); // seed doesn't matter
+
     loop {
         log::info!("Looping...");
 
         for duty_num in SERVO_MIN_DUTY..=SERVO_MAX_DUTY {
+            let next_random = small_rng.gen_range(SERVO_MIN_DUTY..=SERVO_MAX_DUTY);
+            log::info!("Next Random: {}", next_random);
             log::info!("Duty: {}", duty_num);
             servo_1_pwm_channel.set_duty(duty_num).unwrap();
             servo_2_pwm_channel.set_duty(15 - duty_num).unwrap();
